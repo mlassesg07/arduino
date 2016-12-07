a9f93559bcc68b27b17658ca4a86803ada32b346
Magnus Lassesgården <magnus.lassesgarden@gmail.com>
2016-12-07 21:10:40 +0100

    Kommenterat
    Lagt till kommentarer som saknades

--- BlinkandeLED/BlinkandeLED.ino
+++ BlinkandeLED/BlinkandeLED.ino
@@ -5,14 +5,14 @@ const int PinLed4 = 13; //Led4 till pinne 13
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PinLed1, OUTPUT);
-  pinMode(PinLed2, OUTPUT);
-  pinMode(PinLed3, OUTPUT);
-  pinMode(PinLed4, OUTPUT);
+  pinMode(PinLed1, OUTPUT); //Sätter PinLed1 till OUTPUT
+  pinMode(PinLed2, OUTPUT); //Sätter PinLed2 till OUTPUT
+  pinMode(PinLed3, OUTPUT); //Sätter PinLed3 till OUTPUT
+  pinMode(PinLed4, OUTPUT); //Sätter PinLed4 till OUTPUT
 
 }
 
-int delayTime = 1000;
+int delayTime = 1000; //Sätter variabel delayTime som integer
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -27,38 +27,38 @@ void loop() {
     } */
 
   {
-    digitalWrite(PinLed1, HIGH);
-    delay(1000);
-    digitalWrite(PinLed1, LOW);
-    delay(1000);
-    digitalWrite(PinLed2, HIGH);
-    delay(1000);
-    digitalWrite(PinLed2, LOW);
-    delay(1000);
-    digitalWrite(PinLed3, HIGH);
-    delay(1000);
-    digitalWrite(PinLed3, LOW);
-    delay(1000);
-    digitalWrite(PinLed4, HIGH);
-    delay(1000);
-    digitalWrite(PinLed4, LOW);
-    delay(1000);
-    digitalWrite(PinLed1, HIGH);
-    delay(500);
-    digitalWrite(PinLed1, LOW);
-    delay(500);
-    digitalWrite(PinLed2, HIGH);
-    delay(500);
-    digitalWrite(PinLed2, LOW);
-    delay(500);
-    digitalWrite(PinLed3, HIGH);
-    delay(500);
-    digitalWrite(PinLed3, LOW);
-    delay(500);
-    digitalWrite(PinLed4, HIGH);
-    delay(500);
-    digitalWrite(PinLed4, LOW);
-    delay(500);
+    digitalWrite(PinLed1, HIGH);  //Sätter Led1 till hög
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed1, LOW);   //Sätter Led1 till låg
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed2, HIGH);  //Sätter Led2 till hög
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed2, LOW);   //Sätter Led2 till låg
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed3, HIGH);  //Sätter Led3 till hög
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed3, LOW);   //Sätter Led3 till låg
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed4, HIGH);  //Sätter Led4 till hög
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed4, LOW);   //Sätter Led4 till låg
+    delay(1000);                  //Väntar 1sek
+    digitalWrite(PinLed1, HIGH);  //Sätter Led1 till hög
+    delay(500);                   //Väntar 0,5sek
+    digitalWrite(PinLed1, LOW);   //Sätter Led1 till låg
+    delay(500);                   //Väntar 0,5sek
+    digitalWrite(PinLed2, HIGH);  //Sätter Led2 till hög
+    delay(500);                   //Väntar 0,5sek
+    digitalWrite(PinLed2, LOW);   //Sätter Led2 till låg
+    delay(500);                   //Väntar 0,5sek
+    digitalWrite(PinLed3, HIGH);  //Sätter Led3 till hög
+    delay(500);                   //Väntar 0,5sek
+    digitalWrite(PinLed3, LOW);   //Sätter Led3 till låg
+    delay(500);                   //Väntar 0,5sek
+    digitalWrite(PinLed4, HIGH);  //Sätter Led4 till hög
+    delay(500);                   //Väntar 0,5sek
+    digitalWrite(PinLed4, LOW);   //Sätter Led4 till låg
+    delay(500);                   //Väntar 0,5sek
 
   }
 }
