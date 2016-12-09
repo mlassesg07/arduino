94d6ed7a3130280c22eec887fd470cab03aab518
Magnus Lassesgården <magnus.lassesgarden@gmail.com>
2016-12-09 09:57:49 +0100

    Update Termo-hygrometer.ino
    Skrivit en del av programmet för att kunna läsa av delaytime i datorn. #1

--- Termo-hygrometer/Termo-hygrometer.ino
+++ Termo-hygrometer/Termo-hygrometer.ino
@@ -1,11 +1,25 @@
+const int kPinLed = 13;
+
 void setup()
 {
-  
+  pinMode(kPinLed, OUTPUT;
+  Serial.begin(9600);
   }
 
+int delayTime = 1000;
+          
 void loop()
 {
-
-
+    delayTime = delayime - 100;
+    IF(delayTime <= 0){
+      delayTime = 1000;
+		}
+	Serial.print("delayTime = ");
+	Serial.print(delayTime);
+	digitalWrite(kPinLed, HIGH);
+	delay(delayTime);
+	digitalWrite(kPinLed, LOW);
+	delay(delayTime);
 }
+	
 
