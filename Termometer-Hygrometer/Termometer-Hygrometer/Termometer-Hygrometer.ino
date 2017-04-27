c0a3934c702452fe9122c5851f7c5cf0a1958942
Magnus Lassesgården <magnus.lassesgarden@gmail.com>
2017-04-27 14:23:37 +0200

    Update Termometer-Hygrometer.ino
    Justerat filnamn samt flyttat om rader gällande skriva till fil. Måste fortsätta med hur man ska få med datum och tid in i filen för varje läsning.

--- Termometer-Hygrometer/Termometer-Hygrometer/Termometer-Hygrometer.ino
+++ Termometer-Hygrometer/Termometer-Hygrometer/Termometer-Hygrometer.ino
@@ -48,9 +48,9 @@ uint32_t delayMS;
 // library functions will not work.
 const int CSpin = 10;
 String dataString =""; // holds the data to be written to the SD card
-float sensorReading1 = 0.00; // value read from your first sensor
-float sensorReading2 = 0.00; // value read from your second sensor
-float sensorReading3 = 0.00; // value read from your third sensor
+//float sensorReading1 = 0.00; // value read from your first sensor
+//float sensorReading2 = 0.00; // value read from your second sensor
+//float sensorReading3 = 0.00; // value read from your third sensor
 File sensorData;
 //
 //
@@ -127,21 +127,15 @@ void loop() {
   }
 }
 
-void setup()
-{
-//
-void loop(){
-Fortsätta härifrån och plocka in raderna nedanför.
-  // build the data string
-dataString = String(sensorReading1) + "," + String(sensorReading2) + "," + String(sensorReading3); // convert to CSV
+dataString = String(datumochtid) + "," + String(event.temperature) + "," + String(event.relative_humidity) ; // convert to CSV
 saveData(); // save to SD card
 delay(delayMS); // delay before next write to SD Card, adjust as required
 }
 //
 void saveData(){
-if(SD.exists("data.csv")){ // check the card is still there
+if(SD.exists("temp-hyg.csv")){ // check the card is still there
 // now append new data file
-sensorData = SD.open("data.csv", FILE_WRITE);
+sensorData = SD.open("temp-hyg.csv", FILE_WRITE);
 if (sensorData){
 sensorData.println(dataString);
 sensorData.close(); // close the file
@@ -153,6 +147,15 @@ Serial.println("Error writing to file !");
 }
 
 
+
+
+
+//void setup()
+//{
+//
+//void loop(){
+//Fortsätta härifrån och plocka in raderna nedanför.
+  // build the data string
 
 
 
