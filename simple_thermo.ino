e550060ed38c5b1617f9b849687fd180b6df9050
Magnus Lassesgården <magnus.lassesgarden@gmail.com>
2018-12-17 09:53:22 +0100

    Update simple_thermo.ino
    Rensat i koden som inte ska vara med.

--- simple_thermo.ino
+++ simple_thermo.ino
@@ -1,1 +1,36 @@
+const int PinLed2 = 11; //Led1 till pinne 11
 
+const int PinLed3 = 12; //Led2 till pinne 12
+
+const int PinLed4 = 13; //Led3 till pinne 13
+
+
+
+void setup() {
+
+  // put your setup code here, to run once:
+
+  pinMode(PinLed1, OUTPUT); //Sätter PinLed1 till OUTPUT
+
+  pinMode(PinLed2, OUTPUT); //Sätter PinLed2 till OUTPUT
+
+  pinMode(PinLed3, OUTPUT); //Sätter PinLed3 till OUTPUT
+
+ 
+
+}
+
+
+
+int delayTime = 1000; //Sätter variabel delayTime som integer
+
+
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+
+
+
+  
+}
